8b2a3c81e0b2a4904e53a989693327ded353ead2
cms <audtlr93@naver.com>
2023-02-28 18:57:01 +0900

    refactor: Action 의 api 통신 리팩토링

--- client/src/store/user-diary-action.ts
+++ client/src/store/user-diary-action.ts
@@ -2,20 +2,11 @@ import { Dispatch } from "@reduxjs/toolkit";
 import { userDiaryAction } from "./user-diary-slice";
 import { useDispatch } from "react-redux"; // useDispath 의 사전 생성
 import type { AppDispatch } from "./index"; //  action 생성자용 Dispatch 타입
+import { getUserDiary } from "../api/user-diary"; // 유저데이터 api
 
 export const getRequest = (userEmail: string) => {
   return async (dispatch: Dispatch) => {
-    const getUserDiaryData = async () => {
-      const response = await fetch(
-        `http://localhost:5000/api/diary/${userEmail}`
-      );
-
-      const responseData = await response.json();
-
-      return responseData.data;
-    };
-
-    const userDiaryData = await getUserDiaryData();
+    const userDiaryData = await getUserDiary(userEmail);
 
     dispatch(userDiaryAction.putUserDiary(userDiaryData));
   };
